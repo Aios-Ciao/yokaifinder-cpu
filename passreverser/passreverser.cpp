05a5db71c6eb1ac8d5191b1c49ed51a10dc81bf9
Aios-Ciao <thor_ichino@hotmail.co.jp>
2022-01-01 13:52:06 +0900

    add 行リバースツール追加
    行単位で文字の並びを逆順にする

---
+++ passreverser/passreverser.cpp
@@ -0,0 +1,38 @@
+﻿#include <fstream>
+#include <iostream>
+#include <forward_list>
+#include <numeric>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    ifstream ifs;
+    ofstream ofs;
+
+    string infname;
+    string outfname;
+    string password;
+
+    if (argc != 3) {
+        cout << "usage: passreverser infilename.txt outfilename.txt" << endl;
+        return (-1);
+    }
+
+    infname = argv[1];
+    outfname = argv[2];
+
+    ifs.open(infname, ios::in);
+    ofs.open(outfname, ios::out);
+
+    while (getline(ifs, password)) {
+        ofs << string(password.rbegin(), password.rend()) << endl;
+    }
+
+    ifs.close();
+    ofs.close();
+
+    return (0);
+}
